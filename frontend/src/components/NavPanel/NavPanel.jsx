33fa76f095692b7ffce3d9c91a7874bb02a05939
vladyslav.sheruda <vladyslav.sheruda@gmail.com>
2025-01-01 17:53:28 +0100

    Add active state highlighting for NavPanel items
    Replaced the home icon with a more appropriate connection icon. Introduced logic to highlight the active navigation item based on the current route using `useLocation`. Updated CSS to visually distinguish the selected navigation item.

--- frontend/src/components/NavPanel/NavPanel.jsx
+++ frontend/src/components/NavPanel/NavPanel.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './NavPanel.css';
-import { GrHomeRounded } from 'react-icons/gr';
 import { IoDesktopSharp } from 'react-icons/io5';
 import { MdElectricBolt } from 'react-icons/md';
+import { PiPlugsConnectedFill } from "react-icons/pi";
 import { IoIosSave } from 'react-icons/io';
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { useProfile } from '../../context/ProfileContext.jsx';
 
 function NavPanel() {
     const { isSyncing } = useProfile();
+    const location = useLocation();
+
+    const isSelected = path => path === location.pathname;
+
     return (
         <div className={'nav-panel'}>
-            <Link to={'/connection-list'} className={'nav-item'}>
-                <GrHomeRounded title={'Connection List'} />
+            <Link to={'/connection-list'} className={`nav-item ${isSelected('/connection-list') && 'selected'}`}>
+                <PiPlugsConnectedFill title={'Connection List'} />
             </Link>
-            <Link to={'/host-settings'} className={'nav-item'}>
+            <Link to={'/host-settings'} className={`nav-item ${isSelected('/host-settings') && 'selected'}`}>
                 <IoDesktopSharp title={'Host Settings'} />
             </Link>
-            <Link to={'/system-health'} className={'nav-item'}>
+            <Link to={'/system-health'} className={`nav-item ${isSelected('/system-health') && 'selected'}`}>
                 <MdElectricBolt title={'System Health'} />
             </Link>
             {isSyncing && <IoIosSave className={'nav-item sync-progress'} />}
